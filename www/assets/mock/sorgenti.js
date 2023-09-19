cb34d41c059c665fdf2cb47df63c229bac778d58
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2023-09-19 23:34:06 +0200

    aggiornato versione

---
+++ www/assets/mock/sorgenti.js
@@ -0,0 +1,32 @@
+/**
+ *  contenitori di sorgenti
+ */
+
+let sorgenti = {
+    "1": {
+        "stile": {
+            "colore": "#FF0021"
+        }
+    },
+    "2": {
+        "stile": {
+            "colore": "#00DF00"
+        }
+    },
+    "3": {
+        "stile": {
+            "colore": "#EAF"
+        }
+    },
+    "4": {
+        "stile": {
+            "colore": "#DFE"
+        }
+    },
+    "5": {
+        "stile": {
+            "colore": "#890092"
+        }
+    }
+};
+export default sorgenti;
---
+++ www/assets/mock/temiSintetici.js
@@ -0,0 +1,61 @@
+/**
+ *  nuovo formato con definizione del colore del tema
+ */
+
+let temiSintetici = {
+    "4": {
+        "categorie": [10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16],
+        "stile": {
+            "colore": "#FFC921"
+        }
+    },
+    "6": {
+        "categorie": [91,
+            92,
+            94,
+            93,
+            95],
+        "stile": {
+            "colore": "#FDD0B3"
+        }
+    },
+    "5": {
+        "categorie": [500,
+            17,
+            18,
+            20,
+            22,
+            85,
+            86,
+            19,
+            21,
+            23,
+            84,
+            87,
+            88,
+            89,
+            90],
+        "stile": {
+            "colore": "#EA4A3E"
+        }
+    },
+    "6": {
+        "categorie": [95, 91, 94, 93, 92],
+        "stile": {
+            "colore": "#FDD0B3"
+        }
+    },
+    "7": {
+        "categorie": [43, 44],
+        "stile": {
+            "colore": "#006FD2"
+        }
+    }
+};
+export default temiSintetici;
